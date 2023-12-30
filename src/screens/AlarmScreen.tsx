b60808527fb7b346d5326be92c8b7f789c91d49a
hwanheejung <hwanheejung117@gmail.com>
2023-12-30 11:57:19 +0900

    #50 fix: 정리

---
+++ src/screens/AlarmScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const AlarmScreen = () => {
+  return (
+    <View>
+      <Text>AlarmScreen</Text>
+    </View>
+  );
+};
+
+export default AlarmScreen;
