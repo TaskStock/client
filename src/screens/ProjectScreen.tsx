c861f7653df2e60e12f9fc3d6aef0d60b3a397b1
hwanheejung <hwanheejung117@gmail.com>
2024-01-06 23:15:54 +0900

    #88 feat: logout

--- src/screens/ProjectScreen.tsx
+++ src/screens/ProjectScreen.tsx
@@ -1,16 +1,7 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import React from "react";
 import { Text, TouchableOpacity, View } from "react-native";
-
-const checkStorage = async () => {
-  try {
-    const accessToken = await AsyncStorage.getItem("accessToken");
-
-    console.log("Check => Access Token: ", accessToken);
-  } catch (error) {
-    console.error("Error reading values from AsyncStorage", error);
-  }
-};
+import { checkStorage } from "../utils/asyncStorage";
 
 const ProjectScreen = () => {
   return (
