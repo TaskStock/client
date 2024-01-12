f81afd338964ce1bb493ec902926a19553da55ae
newtonjjang <newtonjjang@naver.com>
2024-01-13 02:14:37 +0900

    #128 fix: checkissamelocalday refactoring

--- src/utils/checkIsSameLocalDay.ts
+++ src/utils/checkIsSameLocalDay.ts
@@ -1,23 +1,19 @@
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
-import { IsoString } from "../@types/calendar";
 
 // Extend dayjs with utc plugin
 dayjs.extend(utc);
 
+// 두 utc string이 같은 날인지 확인.
 function checkIsSameLocalDay(utcString1: string, utcString2: string): boolean {
   // Parse UTC strings into dayjs objects
-  const dt1 = dayjs.utc(utcString1);
-  const dt2 = dayjs.utc(utcString2);
+  const dt1 = dayjs(utcString1);
+  const dt2 = dayjs(utcString2);
 
-  // Convert UTC datetime objects to local datetime objects
-  const localDt1 = dt1.local();
-  const localDt2 = dt2.local();
-
-  return localDt1.isSame(localDt2, "day");
+  return dt1.isSame(dt2, "day");
 }
 
-// 어제 6시부터 오늘 6시 사이의 시간인지 확인. todo값을 변경했을시.
+// 어제 6시부터 오늘 6시 사이의 시간인지 확인. 즉 정산날짜에 해당하는 utc인지를 확인.
 function checkIsLocalBetween6to6(utcString: string): boolean {
   // Parse UTC string into dayjs object
   const dt = dayjs.utc(utcString);
