1f21aabd915efb2bacba87ce96b318cf8b622eae
newtonjjang <newtonjjang@naver.com>
2024-02-11 19:07:30 +0900

    #194 fix: 가치 변동률 infinite로 되는부분 임시수정

--- src/utils/calculateUserDiffRate.ts
+++ src/utils/calculateUserDiffRate.ts
@@ -6,13 +6,15 @@ export const calculateUserDiffRate = (user: {
 }) => {
   const data = {
     cumulative_value: user.cumulative_value,
-    value_month_ago: user.value_yesterday_ago,
+    value_yesterday_ago: user.value_yesterday_ago,
   };
 
-  const diff = data.cumulative_value - data.value_month_ago;
+  const toDivide =
+    data.value_yesterday_ago === 0 ? 100 : data.value_yesterday_ago;
+
+  const diff = data.cumulative_value - data.value_yesterday_ago;
   const diff_rate =
-    ((data.cumulative_value - data.value_month_ago) * 100) /
-    data.value_month_ago;
+    ((data.cumulative_value - data.value_yesterday_ago) * 100) / toDivide;
 
   const renderDiffRate = diff_rate.toFixed(2);
 
