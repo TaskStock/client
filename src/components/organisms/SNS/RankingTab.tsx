093550bf9f78513bf36b3f7a5ff8ebd41edbc3b7
hwanheejung <hwanheejung117@gmail.com>
2024-03-05 10:46:34 +0900

    #251 fix: refresh custom

--- src/components/organisms/SNS/RankingTab.tsx
+++ src/components/organisms/SNS/RankingTab.tsx
@@ -1,15 +1,14 @@
-import { useRefresh } from "@react-native-community/hooks";
-import React from "react";
-import { FlatList, RefreshControl, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { FlatList, RefreshControl, TouchableOpacity, View } from "react-native";
 import { useTheme } from "styled-components";
 import { spacing } from "../../../constants/spacing";
 import { useAppDispatch } from "../../../store/configureStore.hooks";
 import { IFriend, getFriendsThunk } from "../../../store/modules/getFriends";
 import FlexBox from "../../atoms/FlexBox";
 import Icons from "../../atoms/Icons";
+import LoadingSpinner from "../../atoms/LoadingSpinner";
 import Text from "../../atoms/Text";
 import UserBox from "../../molecules/SNS/UserBox";
-import { palette } from "../../../constants/colors";
 
 const Filter = ({ onPress, iconColor }) => (
   <TouchableOpacity onPress={onPress} style={{ marginTop: spacing.padding }}>
@@ -27,9 +26,17 @@ const Filter = ({ onPress, iconColor }) => (
 
 const RankingTab = ({ data }) => {
   const dispatch = useAppDispatch();
-  const { isRefreshing, onRefresh } = useRefresh(() =>
-    dispatch(getFriendsThunk())
-  );
+
+  const [isRefreshing, setIsRefreshing] = useState(false);
+
+  const onRefresh = async () => {
+    setIsRefreshing(true);
+    setTimeout(() => {
+      dispatch(getFriendsThunk());
+      setIsRefreshing(false);
+    }, 1000);
+  };
+
   const theme = useTheme();
   return (
     <>
@@ -59,10 +66,26 @@ const RankingTab = ({ data }) => {
           <RefreshControl
             refreshing={isRefreshing}
             onRefresh={onRefresh}
-            tintColor={palette.red}
+            tintColor="transparent"
+            colors={["transparent"]}
+            style={{ backgroundColor: "transparent" }}
           />
         }
         showsVerticalScrollIndicator={false}
+        ListHeaderComponent={
+          <View
+            style={{
+              position: "absolute",
+              width: "100%",
+              height: 60,
+              alignItems: "center",
+              justifyContent: "center",
+              top: -60,
+            }}
+          >
+            <LoadingSpinner />
+          </View>
+        }
       />
     </>
   );
